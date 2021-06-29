7de56ba4a0386d72e08f0f18a17dc75a6f2e4c7e
Marcelo Silva (mac) <desenv.marcelo.silva@gmail.com>
2021-06-29 15:29:27 -0300

    feat: create Dashboard
    Add Summary component

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,33 @@
+import { incomeImg, outcomeImg, totalImg } from '../../assets';
+
+import { Container } from './styles';
+
+export function Summary() {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomeImg} alt="entradas" />
+        </header>
+        <strong>R$ 1.000,00</strong>
+      </div>
+
+      <div>
+        <header>
+          <p>Saidas</p>
+          <img src={outcomeImg} alt="entradas" />
+        </header>
+        <strong>-R$ 500,00</strong>
+      </div>
+
+      <div className="hightlight">
+        <header>
+          <p>Saldo</p>
+          <img src={totalImg} alt="entradas" />
+        </header>
+        <strong>R$ 500,00</strong>
+      </div>
+    </Container>
+  );
+}
