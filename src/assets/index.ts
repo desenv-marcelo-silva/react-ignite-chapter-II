00a648aa2f8f3293d45f102141da8c85c1ef3040
Marcelo Silva (mac) <desenv.marcelo.silva@gmail.com>
2021-06-29 15:28:34 -0300

    feat: create index to load imgs

---
+++ src/assets/index.ts
@@ -0,0 +1,7 @@
+import incomeImg from './income.svg';
+import outcomeImg from './outcome.svg';
+import logoImg from './logo.svg';
+import totalImg from './total.svg';
+import closeImg from './close.svg';
+
+export { incomeImg, outcomeImg, logoImg, totalImg, closeImg };
