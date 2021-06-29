fe6a6ca6dc10dd89cef99e8d819fe887d2fa2429
Marcelo Silva (mac) <desenv.marcelo.silva@gmail.com>
2021-06-29 11:16:48 -0300

    chore: add style (global)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import { GlobalStyle } from './styles/global';
 
 function App() {
-  return <div className="App">Marcelo Silva Money</div>;
+  return (
+    <div className="App">
+      <h1>Marcelo Silva Money</h1>
+      <GlobalStyle />
+    </div>
+  );
 }
 
 export default App;
