7fb335311d07e56e79c328a82a31d4e6d858962b
Marcelo Silva (mac) <desenv.marcelo.silva@gmail.com>
2021-06-29 16:59:56 -0300

    feat: add open/close modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
+import { useState } from 'react';
+
+import Modal from 'react-modal';
+
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
 
+Modal.setAppElement('#root');
+
 function App() {
+  const [isTransactionModalOpen, setIsTransactionModalOpen] = useState(false);
+
+  function handleOpenTransactionModal() {
+    setIsTransactionModalOpen(true);
+  }
+
+  function handleCloseTransactionModal() {
+    setIsTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenTransactionModal={handleOpenTransactionModal} />
       <Dashboard />
+
+      <Modal
+        isOpen={isTransactionModalOpen}
+        onRequestClose={handleCloseTransactionModal}
+      >
+        <h2>Cadatrar transacao</h2>
+      </Modal>
+
       <GlobalStyle />
     </>
   );
