71440d1c9a9a80a17a1d5d9119b0b2bcc2d4924a
Marcelo Silva (mac) <desenv.marcelo.silva@gmail.com>
2021-07-10 10:21:17 -0300

    feat: list data from api

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,37 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance bigdata',
+          type: 'deposit',
+          category: 'Dev',
+          ammount: 1200,
+          createdAt: new Date('2021-03-11 08:35:20'),
+        },
+        {
+          id: 2,
+          title: 'Condomínio',
+          type: 'withdraw',
+          category: 'Casa',
+          ammount: 343.59,
+          createdAt: new Date('2021-03-19 13:55:02'),
+        },
+        {
+          id: 3,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          ammount: 1200,
+          createdAt: new Date('2021-03-19 13:45:38'),
+        },
+      ],
+    });
+  },
+
   routes() {
     this.namespace = 'api';
 
